61879d4ec5b355e81d36005a19115f6ac44be8ba
Jorge Pérez Zerpa <42485529+jorgepz@users.noreply.github.com>
2021-07-06 07:29:04 -0300

    Update test_fat.m

--- tests/srcB/test_fat.m
+++ tests/srcB/test_fat.m
@@ -1,4 +1,4 @@
-function test_suite=test_fac
+function test_suite=test_fat
 % initialize unit tets
     try
         test_functions=localfunctions();
@@ -10,26 +10,26 @@ function test_suite=test_fac
 %     Basic tests     %
 %%%%%%%%%%%%%%%%%%%%%%%
     
-function test_fac_0
+function test_fat_0
 % test if fac(0)==1
-    assertEqual(fac(0),1);
+    assertEqual(fat(0),1);
 
-function test_fac_1
+function test_fat_1
 % test if fac(1)==1
-    assertEqual(fac(1),1);
+    assertEqual(fat(1),1);
     
-function test_fac_5
+function test_fat_5
 % test if fac(5)==120
-    assertEqual(fac(5),120);
+    assertEqual(fat(5),120);
     
 %%%%%%%%%%%%%%%%%%%%%%%
 % More advanced tests %
 %%%%%%%%%%%%%%%%%%%%%%%
 
-function test_fac_exception_negative
+function test_fat_exception_negative
 % test if exceptions are thrown for negative values
-    assertExceptionThrown(@()fac(-1));
+    assertExceptionThrown(@()fat(-1));
     
-function test_fac_exception_noninteger
+function test_fat_exception_noninteger
 % test if exceptions are thrown for noninteger values
-    assertExceptionThrown(@()fac(1.5),'*');
+    assertExceptionThrown(@()fat(1.5),'*');
