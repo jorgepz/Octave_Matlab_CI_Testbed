bdebab7af6185e222a8a39f6c5d8583f4f6e2367
jorgepz <jorgepz@fing.edu.uy>
2021-07-25 14:58:27 -0300

    add call to new function in new folder and edit yml file

--- tests/test_fac.m
+++ tests/test_fac.m
@@ -9,7 +9,7 @@ function test_suite=test_fac
 %%%%%%%%%%%%%%%%%%%%%%%
 %     Basic tests     %
 %%%%%%%%%%%%%%%%%%%%%%%
-    
+
 function test_fac_0
 % test if fac(0)==1
     assertEqual(fac(0),1);
@@ -17,11 +17,14 @@ function test_fac_0
 function test_fac_1
 % test if fac(1)==1
     assertEqual(fac(1),1);
-    
+
 function test_fac_5
 % test if fac(5)==120
     assertEqual(fac(5),120);
-    
+
+function test_fac_6
+  assertEqual(auxiliarFunctionB(2),4);
+
 %%%%%%%%%%%%%%%%%%%%%%%
 % More advanced tests %
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -29,7 +32,7 @@ function test_fac_5
 function test_fac_exception_negative
 % test if exceptions are thrown for negative values
     assertExceptionThrown(@()fac(-1));
-    
+
 function test_fac_exception_noninteger
 % test if exceptions are thrown for noninteger values
     assertExceptionThrown(@()fac(1.5),'*');
