260c3dddfce6605e5df1e97308e314bea75dd50e
jorgepz <jorgepz@fing.edu.uy>
2023-02-22 17:24:57 -0300

    tests

--- test/test_coverage.m
+++ test/test_coverage.m
@@ -10,19 +10,19 @@ function test_1
 % test if fac(0)==1
   assertEqual(fac(0),1);
 
-function test_2
+% function test_2
 % test if fac(1)==1
   assertEqual(fac(1),1);
 
-function test_3
+% function test_3
   assertEqual( dummy_fun_2(2) , 4 );
 
-function test_4
+% function test_4
   addpath('folderB')
   someCalc
   assertEqual( value, 4 );
 
-function test_5
+% function test_5
   addpath('folderB')
   anotherCalc
   assertEqual( value, 9 );
