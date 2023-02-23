228a85e87531f54f3aadc93e5abbdba7fde4d625
jorgepz <jorgepz@fing.edu.uy>
2023-02-23 11:38:34 -0300

    debugging

--- test/moxunit_testWithCoverage.m
+++ test/moxunit_testWithCoverage.m
@@ -11,11 +11,11 @@ function test_suite=moxunit_testWithCoverage
 %   exampleA
 %   assertEqual(result,4);
 
-function test_2 % moving to folder
-  cd(['..' filesep 'examples'])  
-  exampleB
-  assertEqual(result,2);
+% function test_2 % moving to folder
+%   cd(['..' filesep 'examples'])  
+%   exampleB
+%   assertEqual(result,2);
 
-% function test_3 % running from here
-%   run(['..' filesep 'examples'  filesep 'exampleC.m'])
-%   assertEqual(result,27);
+function test_3 % running from here
+  run(['..' filesep 'examples'  filesep 'exampleC.m'])
+  assertEqual(result,27);
