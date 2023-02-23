759ed2107371f9287a9a1a2d2010efc819709b21
jorgepz <jorgepz@fing.edu.uy>
2023-02-23 11:26:55 -0300

    more changes

--- test/moxunit_testWithCoverage.m
+++ test/moxunit_testWithCoverage.m
@@ -6,16 +6,16 @@ function test_suite=moxunit_testWithCoverage
   end
   initTestSuite;
 
-function test_1
-  addpath([ 'examples'])  
+function test_1 % adding folder to path
+  addpath([ '..' filesep 'examples'])  
   exampleA
   assertEqual(result,4);
 
-function test_2
-  cd('examples')  
+function test_2 % moving to folder
+  cd('..' filesep 'examples')  
   exampleB
   assertEqual(result,2);
 
-function test_3
-  run('examples/exampleC.m')
+function test_3 % running from here
+  run('..' filesep 'examples'  filesep 'exampleC.m')
   assertEqual(result,27);
